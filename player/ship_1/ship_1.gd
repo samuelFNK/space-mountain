446ba9ada4884f40759f4946cc6ac92ba829c3e8
samuel.funck <samuel.funck@outlook.com>
2026-02-25 14:48:38 +0100

    basic flying functionality with some physics and logic. Minimal visuals.

--- player/ship_1/ship_1.gd
+++ player/ship_1/ship_1.gd
@@ -11,6 +11,7 @@ var acceleration:float = 6.0
 
 var current_speed:float = 0
 var target_speed:float = 0
+var grounded:bool = false
 
 var vel:Vector3 = Vector3.ZERO
 var turn_input:float = 0
@@ -19,10 +20,17 @@ var pitch_input:float = 0
 func get_input(delta:float):
 	if Input.is_action_pressed("throttle_up"):
 		target_speed = min(current_speed + throttle_delta * delta, max_speed)
-	if Input.is_action_pressed("pitch_down"):
-		target_speed = max(current_speed - throttle_delta * delta, min_speed)
-	turn_input = Input.get_action_strength("roll_left") - Input.get_action_strength("roll_right")
-	pitch_input = Input.get_action_strength("pitch_up") - Input.get_action_strength("pitch_down")
+	if Input.is_action_pressed("throttle_down"):
+		var min_limit:float = 0.0 if grounded else min_speed
+		target_speed = max(current_speed - throttle_delta * delta, min_limit)
+	turn_input = 0
+	if current_speed > 0.5: 
+		turn_input = Input.get_action_strength("roll_left") - Input.get_action_strength("roll_right")
+	pitch_input = 0
+	if not grounded:
+		pitch_input -= Input.get_action_strength("pitch_down") 
+	if current_speed >= 1:
+		pitch_input += Input.get_action_strength("pitch_up")
 	
 func _physics_process(delta:float) -> void:
 	get_input(delta)
@@ -31,9 +39,22 @@ func _physics_process(delta:float) -> void:
 	transform.basis = transform.basis.rotated(transform.basis.x, pitch_input * pitch_speed * delta)
 	#ROTATION
 	transform.basis = transform.basis.rotated(Vector3.UP, turn_input * turn_speed * delta)
-	$MeshInstance3D.rotation.z = lerp($MeshInstance3D.rotation.z, turn_input, level_speed * delta)
+	if grounded:
+		$MeshInstance3D.rotation.z = 0
+	else:
+		$MeshInstance3D.rotation.z = lerp($MeshInstance3D.rotation.z, turn_input, level_speed * delta)
 	#VELOCITY
 	current_speed = lerp(current_speed, target_speed, acceleration * delta)
 	vel = -transform.basis.z * current_speed
 	velocity = vel
+	
+	if is_on_floor():
+		if not grounded:
+			rotation.x = 0
+		grounded = true
+		vel.y -= 1
+		velocity = vel
+	else:
+		grounded = false
+		
 	move_and_slide()
